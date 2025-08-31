db5dcfcd2beb54f207cbf5a830fbeed1a7cd95f7
paul-1 <6473457+paul-1@users.noreply.github.com>
2025-08-31 18:39:23 -0400

    Update zipfile to include version

--- release.pl
+++ release.pl
@@ -26,6 +26,7 @@ print("sha    : ", $digest->hexdigest, "\n");
 
 $url .= "/$zipfile";
 $repo->{applets}[0]->{applet}[0]->{url}[0] = $url;
+print("url    : $url");
 
 XMLout($repo, RootName => 'extensions', NoSort => 1, XMLDecl => 1, KeyAttr => '', OutputFile => $repofile, NoAttr => 0);
 
